5be74061a5af7bc820c8f204edf2f157be3f7639
domenico094 <domy-and94@outlook.com>
2022-07-22 18:35:26 +0200

    add timer top bar cube ecc..

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <string>
 #include "tga.h"
 using namespace std;
 
@@ -33,14 +34,25 @@ const int nTexture = 5;
 GLuint textures[nTexture];
 const char *textureFile[nTexture] = {"texture/floor.tga", "texture/wall.tga", "texture/ceiling.tga", "texture/cubo.tga", "texture/earth.tga"};
 
+// timer
+int timer = 300; // 300 sec , 5 minuti
+string strTimer = "";
+
+int timerCloseWindow = 50;
+string strTimerCloseWindow = "";
+
 void inizializza();
 void init();
 void reshape(int, int);
+void topDisplay();
 void mainDisplay();
 void drawLabyrinth();
 void texture();
 void keyboardSpecial(int, int, int);
 void keyboard(unsigned char, int, int);
+void TimeRotation(int);
+void Timer(int);
+
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
@@ -56,11 +68,12 @@ int main(int argc, char **argv)
     glutKeyboardFunc(keyboard);
     glutSpecialFunc(keyboardSpecial);
     glutReshapeFunc(reshape);
-
+    TimeRotation(25);
+    Timer(1000);
     texture();
 
-    // int subTopWindow = glutCreateSubWindow(mainWindow,0,0,2000,20);
-    // glutDisplayFunc(topDisplay);
+    int subTopWindow = glutCreateSubWindow(mainWindow, 0, 0, 2000, 20);
+    glutDisplayFunc(topDisplay);
 
     glutMainLoop();
     glDeleteTextures(nTexture, textures);
@@ -153,9 +166,61 @@ void mainDisplay()
     glutSwapBuffers();
 }
 
+// printbitmap
+// Prints the given string at the given raster position
+// using GLUT bitmap fonts.
+// You probably don't want any rotations in the model/view
+// transformation when calling this function.
+void printbitmap(const string msg, double x, double y)
+{
+    glColor3f(1.0f, 1.0f, 1.0f); // sets color
+    glRasterPos2d(x, y);
+    for (string::const_iterator ii = msg.begin(); ii != msg.end(); ++ii)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *ii);
+    }
+}
+
 void topDisplay()
 {
-    // barra con timer , comandi...
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+
+    if (!lose && !win)
+    {
+        glClearColor(0.0, 0.0, 1.0, 0.0); // dark blue
+
+        printbitmap("Timer:", -0.99, -0.5);
+        printbitmap(strTimer, -0.92, -0.5);
+
+        printbitmap("sec  |  f : FULL/NORMAL SCREEN  |  w : WALL/NOT WALL  |  e : EASY/NORMAL  |  esc : QUIT", -0.88, -0.5); 
+    }
+    else if (win)
+    {
+        glClearColor(0.0, 1.0, 0.0, 1.0); // green
+        printbitmap("HAI VINTO      |    esc : QUIT", -0.99, -0.5);
+        printbitmap("TIMER CHIUSURA FINESTRA: ", -0.70, -0.5);
+         printbitmap(strTimerCloseWindow, -0.42, -0.5);
+         printbitmap("sec", -0.38, -0.5);
+    }
+    else if (lose)
+    {
+        glClearColor(1.0, 0.0, 0.0, 0.0); // red
+
+        printbitmap("HAI PERSO      |    esc : QUIT", -0.99, -0.5); 
+          printbitmap("TIMER CHIUSURA FINESTRA: ", -0.70, -0.5);
+         printbitmap(strTimerCloseWindow, -0.42, -0.5);
+         printbitmap("sec", -0.38, -0.5);
+    }
+
+    gluLookAt(eyeX, eyeX, 19.5, eyeX + 0.1, eyeY, eyeZ, 0.0,
+              0.0, -1.0);
+
+    glPopMatrix();
+
+    glutSwapBuffers();
+    glutPostRedisplay();
 }
 
 // texture
@@ -211,6 +276,11 @@ bool collision(bool flag)
     }
     else if (mappa[x][y] == 1 && !wall)
     {
+        return true;
+    }
+    else if (mappa[x + 1][y + 1] == 2 || mappa[x + 1][y] == 2 || mappa[x][y + 1] == 2 || mappa[x][y] == 2)
+    {
+        win = true;
         return true;
     }
     return false;
@@ -275,158 +345,346 @@ void keyboard(unsigned char key, int x, int y)
             wall = true;
         glutPostRedisplay();
         break;
+
     case 'f':
         if (!fullScreen)
         {
             fullScreen = true;
             glutFullScreen();
         }
-            glutPostRedisplay();
-
-            break;
-
-        default:
-            break;
-        }
-    }
-
-    // drawLabirynt
-    void drawFloor(int i, int j)
-    {
-
-        i = i * proporzione;
-        j = j * proporzione;
-        int ip = i + proporzione;
-        int jp = j + proporzione;
-
-        glBindTexture(GL_TEXTURE_2D, textures[0]);
-
-        glBegin(GL_QUADS);
-        glNormal3f(0, 0, 1);
-        glTexCoord2f(0, 0);
-        glVertex3f(jp, i, 0.0);
-
-        glTexCoord2f(1, 0);
-        glVertex3f(j, i, 0.0);
-
-        glTexCoord2f(1, 1);
-        glVertex3f(j, ip, 0.0);
-
-        glTexCoord2f(0, 1);
-        glVertex3f(jp, ip, 0.0);
-        glEnd();
-    }
-
-    void drawCeiling(int i, int j)
-    {
-        i = i * proporzione;
-        j = j * proporzione;
-        int ip = i + proporzione;
-        int jp = j + proporzione;
-
-        glBindTexture(GL_TEXTURE_2D, textures[2]);
-
-        glBegin(GL_QUADS);
-        glNormal3f(0, 0, 1);
-        glTexCoord2f(0, 0);
-        glVertex3f(jp, i, proporzione);
-
-        glTexCoord2f(1, 0);
-        glVertex3f(j, i, proporzione);
-
-        glTexCoord2f(1, 1);
-        glVertex3f(j, ip, proporzione);
-
-        glTexCoord2f(0, 1);
-        glVertex3f(jp, ip, proporzione);
-        glEnd();
-    }
-
-    void drawWall(int i, int j)
-    {
-
-        i = i * proporzione;
-        j = j * proporzione;
-        int ip = i + proporzione;
-        int jp = j + proporzione;
-        glBindTexture(GL_TEXTURE_2D, textures[1]);
-
-        glBegin(GL_QUADS);
-
-        // Parte superiore dei muri => "coperchio cubo"
-        glNormal3fv(normalUp);
-        glTexCoord2f(1, 0);
-        glVertex3f(j, i, proporzione);
-        glTexCoord2f(1, 1);
-        glVertex3f(j, ip, proporzione);
-        glTexCoord2f(0, 1);
-        glVertex3f(jp, ip, proporzione);
-        glTexCoord2f(0, 0);
-        glVertex3f(jp, i, proporzione);
-
-        glNormal3fv(normalFront);
-        glTexCoord2f(1, 1);
-        glVertex3f(jp, ip, proporzione);
-        glTexCoord2f(0, 1);
-        glVertex3f(j, ip, proporzione);
-        glTexCoord2f(0, 0);
-        glVertex3f(j, ip, -proporzione);
-        glTexCoord2f(1, 0);
-        glVertex3f(jp, ip, -proporzione);
-
-        glNormal3fv(normalLeft);
-        glTexCoord2f(1, 1);
-        glVertex3f(j, ip, proporzione);
-        glTexCoord2f(0, 1);
-        glVertex3f(j, i, proporzione);
-        glTexCoord2f(0, 0);
-        glVertex3f(j, i, -proporzione);
-        glTexCoord2f(1, 0);
-        glVertex3f(j, ip, -proporzione);
-
-        // lato
-
-        glNormal3fv(normalFront);
-        glTexCoord2f(0, 0);
-        glVertex3f(j, i, -proporzione);
-        glTexCoord2f(1, 0);
-        glVertex3f(jp, i, -proporzione);
-        glTexCoord2f(1, 1);
-        glVertex3f(jp, i, proporzione);
-        glTexCoord2f(0, 1);
-        glVertex3f(j, i, proporzione);
-
-        // lato
-        glNormal3fv(normalRight);
-        glTexCoord2f(0, 1);
-        glVertex3f(jp, ip, proporzione);
-        glTexCoord2f(1, 1);
-        glVertex3f(jp, i, proporzione);
-        glTexCoord2f(1, 0);
-        glVertex3f(jp, i, -proporzione);
-        glTexCoord2f(0, 0);
-        glVertex3f(jp, ip, -proporzione);
-        glEnd();
-    }
-    void drawLabyrinth()
-    {
-        for (unsigned int i = 0; i < dim; ++i)
+        else if (fullScreen)
         {
-            for (unsigned int j = 0; j < dim; ++j)
+            fullScreen = false;
+
+            glutReshapeWindow(900, 800);
+        }
+        glutPostRedisplay();
+        break;
+    case 'e':
+        if (easy == false)
+        {
+            easy = true;
+            eyeZ = 19.2;
+            glDisable(GL_FOG);
+        }
+        else
+        {
+            easy = false;
+            eyeZ = 1.0;
+            glEnable(GL_FOG);
+        }
+        glutPostRedisplay();
+        break;
+
+    default:
+        break;
+    }
+    if(key == 27)
+        exit(0);
+}
+
+// drawLabirynt
+void drawFloor(int i, int j)
+{
+
+    i = i * proporzione;
+    j = j * proporzione;
+    int ip = i + proporzione;
+    int jp = j + proporzione;
+
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+
+    glBegin(GL_QUADS);
+    glNormal3f(0, 0, 1);
+    glTexCoord2f(0, 0);
+    glVertex3f(jp, i, 0.0);
+
+    glTexCoord2f(1, 0);
+    glVertex3f(j, i, 0.0);
+
+    glTexCoord2f(1, 1);
+    glVertex3f(j, ip, 0.0);
+
+    glTexCoord2f(0, 1);
+    glVertex3f(jp, ip, 0.0);
+    glEnd();
+}
+
+void drawCeiling(int i, int j)
+{
+    i = i * proporzione;
+    j = j * proporzione;
+    int ip = i + proporzione;
+    int jp = j + proporzione;
+
+    glBindTexture(GL_TEXTURE_2D, textures[2]);
+
+    glBegin(GL_QUADS);
+    glNormal3f(0, 0, 1);
+    glTexCoord2f(0, 0);
+    glVertex3f(jp, i, proporzione);
+
+    glTexCoord2f(1, 0);
+    glVertex3f(j, i, proporzione);
+
+    glTexCoord2f(1, 1);
+    glVertex3f(j, ip, proporzione);
+
+    glTexCoord2f(0, 1);
+    glVertex3f(jp, ip, proporzione);
+    glEnd();
+}
+
+void drawWall(int i, int j)
+{
+
+    i = i * proporzione;
+    j = j * proporzione;
+    int ip = i + proporzione;
+    int jp = j + proporzione;
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+
+    glBegin(GL_QUADS);
+
+    // Parte superiore dei muri => "coperchio cubo"
+    glNormal3fv(normalUp);
+    glTexCoord2f(1, 0);
+    glVertex3f(j, i, proporzione);
+    glTexCoord2f(1, 1);
+    glVertex3f(j, ip, proporzione);
+    glTexCoord2f(0, 1);
+    glVertex3f(jp, ip, proporzione);
+    glTexCoord2f(0, 0);
+    glVertex3f(jp, i, proporzione);
+
+    glNormal3fv(normalFront);
+    glTexCoord2f(1, 1);
+    glVertex3f(jp, ip, proporzione);
+    glTexCoord2f(0, 1);
+    glVertex3f(j, ip, proporzione);
+    glTexCoord2f(0, 0);
+    glVertex3f(j, ip, -proporzione);
+    glTexCoord2f(1, 0);
+    glVertex3f(jp, ip, -proporzione);
+
+    glNormal3fv(normalLeft);
+    glTexCoord2f(1, 1);
+    glVertex3f(j, ip, proporzione);
+    glTexCoord2f(0, 1);
+    glVertex3f(j, i, proporzione);
+    glTexCoord2f(0, 0);
+    glVertex3f(j, i, -proporzione);
+    glTexCoord2f(1, 0);
+    glVertex3f(j, ip, -proporzione);
+
+    // lato
+
+    glNormal3fv(normalFront);
+    glTexCoord2f(0, 0);
+    glVertex3f(j, i, -proporzione);
+    glTexCoord2f(1, 0);
+    glVertex3f(jp, i, -proporzione);
+    glTexCoord2f(1, 1);
+    glVertex3f(jp, i, proporzione);
+    glTexCoord2f(0, 1);
+    glVertex3f(j, i, proporzione);
+
+    // lato
+    glNormal3fv(normalRight);
+    glTexCoord2f(0, 1);
+    glVertex3f(jp, ip, proporzione);
+    glTexCoord2f(1, 1);
+    glVertex3f(jp, i, proporzione);
+    glTexCoord2f(1, 0);
+    glVertex3f(jp, i, -proporzione);
+    glTexCoord2f(0, 0);
+    glVertex3f(jp, ip, -proporzione);
+    glEnd();
+}
+GLfloat l = 1.0;
+void drawCube(int i, GLfloat p)
+{
+
+    GLfloat h = l / 2;
+    GLfloat x1 = h * p;
+    GLfloat yh1 = (-1 * h) * p;
+    GLfloat x2 = (-1 * h) * p;
+    GLfloat yh2 = (h)*p;
+    GLfloat x3 = (-1 * h) * p;
+    GLfloat yh3 = (-1 * h) * p;
+    GLfloat x4 = h * p;
+    GLfloat yh4 = h * p;
+    GLfloat l1 = l - (h * p);
+    GLfloat l2 = l + (h * p);
+    glBindTexture(GL_TEXTURE_2D, textures[i]);
+
+    glBegin(GL_QUADS);
+    glNormal3f(-1, 0, 0);
+    glTexCoord2f(1, 0);
+    glVertex3f(x1, yh1, l1);
+    glTexCoord2f(1, 1);
+    glVertex3f(x1, yh1, l2);
+    glTexCoord2f(0, 1);
+    glVertex3f(x3, yh3, l2);
+    glTexCoord2f(0, 0);
+    glVertex3f(x3, yh3, l1);
+    glEnd();
+    glBegin(GL_QUADS);
+    glNormal3f(0, 1, 0);
+    glTexCoord2f(1, 0);
+    glVertex3f(x3, yh3, l1);
+    glTexCoord2f(1, 1);
+    glVertex3f(x3, yh3, l2);
+    glTexCoord2f(0, 1);
+    glVertex3f(x2, yh2, l2);
+    glTexCoord2f(0, 0);
+    glVertex3f(x2, yh2, l1);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glNormal3f(0, -1, 0);
+    glTexCoord2f(0, 0);
+    glVertex3f(x2, yh2, l1);
+    glTexCoord2f(0, 1);
+    glVertex3f(x2, yh2, l2);
+    glTexCoord2f(1, 1);
+    glVertex3f(x4, yh4, l2);
+    glTexCoord2f(1, 0);
+    glVertex3f(x4, yh4, l1);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glNormal3f(-1, 0, 0);
+    glTexCoord2f(0, 0);
+    glVertex3f(x4, yh4, l1);
+    glTexCoord2f(0, 1);
+    glVertex3f(x4, yh4, l2);
+    glTexCoord2f(1, 1);
+    glVertex3f(x1, yh1, l2);
+    glTexCoord2f(1, 0);
+    glVertex3f(x1, yh1, l1);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glNormal3f(0, 0, 1);
+    glTexCoord2f(0, 0);
+    glVertex3f(x1, yh1, l2);
+    glTexCoord2f(0, 1);
+    glVertex3f(x3, yh3, l2);
+    glTexCoord2f(1, 1);
+    glVertex3f(x2, yh2, l2);
+    glTexCoord2f(1, 0);
+    glVertex3f(x4, yh4, l2);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glNormal3f(0, 0, -1);
+    glTexCoord2f(0, 0);
+    glVertex3f(x1, yh1, l1);
+    glTexCoord2f(0, 1);
+    glVertex3f(x3, yh3, l1);
+    glTexCoord2f(1, 1);
+    glVertex3f(x2, yh2, l1);
+    glTexCoord2f(1, 0);
+    glVertex3f(x4, yh4, l1);
+    glEnd();
+}
+float dimensioneSferaT = 1.0;
+void trasformIntoSphere()
+{
+
+    drawCube(3, 1);
+
+    if (dimensioneSferaT < 0.8)
+        dimensioneSferaT += 0.005;
+
+    glBindTexture(GL_TEXTURE_2D, textures[4]);
+
+    glTranslatef(0, 0, 1);
+
+    GLUquadric *q = gluNewQuadric();
+    gluQuadricTexture(q, true);
+    gluSphere(q, dimensioneSferaT, 200, 200);
+}
+void handlerCube(int i, int j)
+{
+
+    glPushMatrix();
+
+    glTranslatef((j * proporzione), (i * proporzione), 0);
+    glRotatef(GLfloat(rotate), 0.0, 0.0, -0.1);
+
+    if (win)
+    {
+        trasformIntoSphere();
+    }
+    else
+    {
+
+        drawCube(3, 1);
+    }
+
+    glPopMatrix();
+
+    glutPostRedisplay();
+}
+void drawLabyrinth()
+{
+    for (unsigned int i = 0; i < dim; ++i)
+    {
+        for (unsigned int j = 0; j < dim; ++j)
+        {
+            drawFloor(i, j);
+            if (eyeZ <= 1.0)
+                drawCeiling(j, i);
+            if (mappa[i][j] == 3)
             {
-                drawFloor(i, j);
-                if (eyeZ <= 1.0)
-                    drawCeiling(j, i);
-                if (mappa[i][j] == 3)
-                {
-                    drawWall(j, i);
-                }
-                else if (mappa[i][j] == 1 && !wall)
-                {
-                    drawWall(j, i);
-                }
+                drawWall(j, i);
+            }
+            else if (mappa[i][j] == 1 && !wall)
+            {
+                drawWall(j, i);
+            }
+            if (mappa[i][j] == 2)
+            {
+                handlerCube(j, i);
             }
         }
     }
+}
 
-    // timer
+// timer
+
+void TimeRotation(int secondi)
+{
+    rotate = (rotate + 4) % 360;
+
+    glutTimerFunc(secondi, TimeRotation, secondi);
+}
+
+void Timer(int sec)
+{
+    if (!win && !lose)
+    {
+        if (timer > 0)
+        {
+            timer--;
+        }
+        else
+        {
+            // gioco = false;
+            lose = true;
+        }
+    }
+     if (lose || win)
+    {
+        if (timerCloseWindow > 0)
+            timerCloseWindow--;
+        else
+            exit(0);
+    }
+    strTimer = to_string(timer);
+    strTimerCloseWindow = to_string(timerCloseWindow);
+    cout << "Rimane" << timer << endl;
+    glutTimerFunc(sec, Timer, sec);
+}
